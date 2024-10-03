6e037fd21cda7890c6eb4969ce64749755d4d3d0
owlHJ <rlaguswns901@gmail.com>
2024-10-04 01:28:34 +0900

    [Silver II] Title: N과 M (9), Time: 16 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+bool visited[9];
+int N, M;
+vector<int>v;
+vector<int>answer;
+void Back(int cnt)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++)
+			cout << answer[i] << " ";
+		cout << '\n';
+		return;
+	}
+	int tmp = -1;
+	for (int i = 0; i < N; i++)
+	{
+		if (!visited[i] && tmp != v[i])
+		{
+			tmp = v[i];
+			answer.push_back(v[i]);
+			visited[i] = true;
+			Back(cnt + 1);
+			answer.pop_back();
+			visited[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end()); // 오름차순 정렬
+
+	Back(0);
+}
