f3768ba50814002ad43b5c81324da65320a0f990
owlHJ <rlaguswns901@gmail.com>
2024-11-21 00:24:18 +0900

    [Silver II] Title: 나무 자르기, Time: 604 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -5,49 +5,43 @@ using namespace std;
 
 int main()
 {
-    int n, m;
+    int n,m;
 
-    // 입력 받기
-    cin >> n >> m;
-    vector<int> vTree(n);
-    for (int i = 0; i < n; ++i)
+    cin>>n>>m;
+
+    vector<int>vTree(n);
+
+    for(int i=0;i<n;++i)
+        cin>>vTree[i];
+    
+    sort(vTree.begin(),vTree.end());
+
+    int low = 0;
+    int result  = 0;
+    int high = vTree[n-1];
+
+
+    while(low<=high)
     {
-        cin >> vTree[i];
-    }
+        int mid = (low + high) / 2;
 
-    // 나무 높이 정렬
-    sort(vTree.begin(), vTree.end());
-
-    // 이진 탐색 초기화
-    int low = 0;                  // 최소 높이
-    int high = vTree[n - 1];      // 최대 높이
-    int result = 0;               // 최적 높이 저장
-
-    while (low <= high)
-    {
-        int mid = (low + high) / 2;  // 현재 중간 높이
-        long long wood = 0;         // 얻을 수 있는 나무 길이
-
-        // 현재 높이에서 얻을 수 있는 나무 길이 계산
+        long long wood = 0;
         for (int i = 0; i < n; ++i)
         {
             if (vTree[i] > mid)
                 wood += (vTree[i] - mid);
         }
+         
 
-        // 조건에 따라 탐색 범위 조정
-        if (wood >= m)  // 필요한 나무 길이를 만족함
+        if(wood >= m)
         {
-            result = mid;   // 현재 높이를 저장
-            low = mid + 1;  // 더 높은 높이로 탐색
+            result = mid;
+            low = mid + 1;
         }
-        else  // 필요한 나무 길이를 만족하지 못함
+        else
         {
-            high = mid - 1; // 더 낮은 높이로 탐색
+            high = mid - 1;
         }
     }
-
-    // 결과 출력
-    cout << result << endl;
-    return 0;
+    cout<<result<<endl;
 }
